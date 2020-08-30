73308c8e8664f0a349a8c7f158b2c122907bfe39
Tulio Parreiras <tulio@usemobile.xyz>
2020-08-30 13:16:51 -0300

    Make UserModel fields optional to make things simple

--- Keychain Store/Keychain Store/UserModel.swift
+++ Keychain Store/Keychain Store/UserModel.swift
@@ -10,24 +10,11 @@ import Foundation
 
 final class UserModel: Encodable {
     
-    var name: String
-    var email: String
-    var fullAddres: String
-    var city: String
-    var state: String
-    var country: String
-    
-    init(name: String, email: String, fullAddres: String, city: String, state: String, country: String) {
-        self.name = name
-        self.email = email
-        self.fullAddres = fullAddres
-        self.city = city
-        self.state = state
-        self.country = country
-    }
-    
-    convenience init() {
-        self.init(name: "", email: "", fullAddres: "", city: "", state: "", country: "")
-    }
+    var name: String?
+    var email: String?
+    var fullAddres: String?
+    var city: String?
+    var state: String?
+    var country: String?
     
 }
