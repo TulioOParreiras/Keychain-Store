d9c200d8b841106afc8fa7b4e6224b692950eaa4
Tulio Parreiras <tulio@usemobile.xyz>
2020-08-30 12:14:42 -0300

    Connect `ViewController` IBOutlets and IBActions

--- Keychain Store/Keychain Store/ViewController.swift
+++ Keychain Store/Keychain Store/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldFullAddress: UITextField!
+    @IBOutlet weak var textFieldCity: UITextField!
+    @IBOutlet weak var textFieldState: UITextField!
+    @IBOutlet weak var textFieldCountry: UITextField!
+    
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    
+    // MARK: - IBActions
+    
+    @IBAction func buttonClearAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func buttonSaveAction(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
