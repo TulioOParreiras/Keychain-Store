9166fe7836b26816ae8bdba33fa88f7e3061a6d5
Tulio Parreiras <tulio@usemobile.xyz>
2020-08-30 12:39:15 -0300

    Add empty `KeychainStore` object

---
+++ Keychain Store/Keychain Store/KeychainStore.swift
@@ -0,0 +1,15 @@
+//
+//  KeychainStore.swift
+//  Keychain Store
+//
+//  Created by Tulio Parreiras on 30/08/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+final class KeychainStore {
+    
+    
+    
+}
