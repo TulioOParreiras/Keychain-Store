87dbc35a158d82d5f1d254fee512297ea3c32f6c
Tulio Parreiras <tulio@usemobile.xyz>
2020-08-30 12:46:59 -0300

    Add save methdo to `KeychainStore`

--- Keychain Store/Keychain Store/KeychainStore.swift
+++ Keychain Store/Keychain Store/KeychainStore.swift
@@ -10,6 +10,34 @@ import Foundation
 
 final class KeychainStore {
     
+    enum KeychainStoreError: Error {
+        case duplicatedItem
+        case unexpectedError
+    }
     
+    static func save(data: Data, forKey key: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        let encodedKey = key.data(using: .utf8)
+        let serviceName = Bundle.main.bundleIdentifier ?? "KeychainStore"
+        let query: [String: Any?] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: serviceName,
+            kSecAttrGeneric as String: encodedKey,
+            kSecAttrAccount as String: encodedKey,
+            kSecAttrAccessible as String: kSecAttrAccessibleWhenUnlocked
+        ]
+        
+        var result: CFTypeRef?
+        let status: OSStatus = SecItemAdd(query as CFDictionary, &result)
+        
+        if status == errSecSuccess {
+            completion(.success(()))
+        } else if status == errSecDuplicateItem {
+            completion(.failure(KeychainStoreError.duplicatedItem))
+        } else if let error = result?.error, let _error = error {
+            completion(.failure(_error))
+        } else {
+            completion(.failure(KeychainStoreError.unexpectedError))
+        }
+    }
     
 }
