1bc964bbdb3902d8cad57f74144d465f1299aa93
Tulio Parreiras <tulio@usemobile.xyz>
2020-08-30 12:31:26 -0300

    Add empty `UserModel`

---
+++ Keychain Store/Keychain Store/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  Keychain Store
+//
+//  Created by Tulio Parreiras on 30/08/20.
+//  Copyright © 2020 Tulio Parreiras. All rights reserved.
+//
+
+import Foundation
+
+final class UserModel {
+    
+    
+}
